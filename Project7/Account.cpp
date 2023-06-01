0b45897262da1915a0f50a3f7584a7dfdcc2a9eb
sssikha <gktmddlr@gmail.com>
2023-06-01 14:24:33 +0900

    welfare & interest logic complete

--- Project7/Account.cpp
+++ Project7/Account.cpp
@@ -17,6 +17,27 @@ public:
 	{
 		balance -= money;
 	}
+	void welfare()
+	{
+		balance = (balance * (1 + interest_ / 100));
+	}
+	void setInterest(double interest)
+	{
+		interest_ = interest;
+	}
+	int getInterest()
+	{
+		return static_cast<int>(interest_);
+	}
+	int getBalanceAfterYears(int n)
+	{
+		for(int i = 0; i < n; i++)
+		{
+			welfare();
+		}
+		return getBalance();
+	}
 private:
+	double interest_ = 5;
 	int balance = 0;
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -21,3 +21,25 @@ TEST_F(AccountFixture, Withdraw) {
 	account.withdraw(600);
 	EXPECT_EQ(9400, account.getBalance());
 }
+
+TEST_F(AccountFixture, Welfare5){
+	account.welfare();
+	EXPECT_EQ(10500, account.getBalance());
+}
+
+TEST_F(AccountFixture, SetInterest) {
+	account.setInterest(10);
+	EXPECT_EQ(10, account.getInterest());
+}
+
+TEST_F(AccountFixture, Welfare10) {
+	account.setInterest(10);
+	account.welfare();
+	EXPECT_EQ(11000, account.getBalance());
+}
+
+TEST_F(AccountFixture, Welfare10After10Years) {
+	account.setInterest(10);
+	account.getBalanceAfterYears(3);
+	EXPECT_EQ(13310, account.getBalance());
+}
